2243fc58bbffd3560efa6ac968b9784fb28484f7
Anton Akuev <antonakuev945@mail.ru>
2023-11-29 13:30:39 +0300

    feat: Создал модель покупателя

---
+++ customers/models/customers.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from phonenumber_field.formfields import PhoneNumberField
+
+from customers.managers import CustomCustomerManager
+
+
+class Customer(AbstractUser):
+    """Модель Покупателя"""
+
+    username = models.CharField(
+        'Никнейм',
+        max_length=32,
+        unique=True,
+        null=True,
+        blank=True,
+    )
+    email = models.EmailField(
+        'Почта',
+        unique=True,
+        null=True,
+        blank=True,
+    )
+    phone_number = PhoneNumberField(
+        'Телефон',
+        unique=True,
+        null=True,
+        blank=True,
+    )
+
+    objects = CustomCustomerManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = 'Покупатель'
+        verbose_name_plural = 'Покупатели'
+
+    @property
+    def full_name(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self) -> str:
+        return f'{self.full_name} ({self.pk})'
