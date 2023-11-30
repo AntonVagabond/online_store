6b32e9d1f7fd69b5a476f584b5e775c59e6f504a
Anton Akuev <antonakuev945@mail.ru>
2023-11-30 15:19:07 +0300

    feat: Добавил доп. модели для модели Product.

---
+++ products/models/products.py
@@ -0,0 +1,101 @@
+from django.db import models
+
+from common.models.mixins import BaseModel
+
+
+class Product(BaseModel):
+    """Модель товара"""
+
+    name = models.CharField(
+        'Название товара',
+        max_length=255,
+    )
+    images = models.ForeignKey(
+        to='ProductImage',
+        on_delete=models.PROTECT,
+        related_name='products',
+        verbose_name='Изображения продукта'
+    )
+    availability = models.BooleanField(
+        verbose_name='Наличие товара',
+        null=True,
+        blank=True,
+        default=True,
+    )
+    price = models.DecimalField(
+        verbose_name='Цена товара',
+        max_digits=8,
+        decimal_places=1,
+        null=True,
+        blank=True,
+    )
+    quantity = models.PositiveSmallIntegerField(
+        verbose_name='Количество товара',
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Время создания',
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('-created_at',)
+
+    def __str__(self) -> models.CharField:
+        return self.name
+
+
+class ProductImage(BaseModel):
+    """Модель изображения товара"""
+
+    image = models.ImageField(
+        verbose_name='Изображение товара',
+        upload_to='products/%Y/%m/%d',
+        null=True,
+        blank=True,
+    )
+
+
+class ProductDescription(BaseModel):
+    """Модель описания товара"""
+
+    product = models.OneToOneField(
+        to='Product',
+        on_delete=models.CASCADE,
+        related_name='products_description',
+        verbose_name='Описание товара',
+        primary_key=True,
+    )
+    description = models.TextField('Описание товара')
+
+
+class ProductFeature(BaseModel):
+    """Модель характеристик товара"""
+
+    product = models.OneToOneField(
+        to='Product',
+        on_delete=models.CASCADE,
+        related_name='products_feature',
+        verbose_name='Характеристика товара',
+        primary_key=True,
+    )
+    size = models.PositiveSmallIntegerField(
+        verbose_name='Размер товара',
+        null=True,
+        blank=True,
+    )
+    colour = models.CharField(
+        'Цвет товара',
+        max_length=30,
+        null=True,
+        blank=True,
+    )
+    patterns = models.BooleanField(
+        verbose_name='Узоры у товара',
+        null=True,
+        blank=True,
+    )
