3ec27bbad340d8314653c9644f2b019a7b20394e
Anton Akuev <antonakuev945@mail.ru>
2023-11-30 19:38:41 +0300

    feat: Добавление модель поставщика.

---
+++ products/models/providers.py
@@ -0,0 +1,29 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+from common.models.mixins import BaseModel
+
+
+class Provider(BaseModel):
+    name = models.CharField(
+        'Название поставщика',
+        max_length=30,
+    )
+    email = models.EmailField(
+        'Почта поставщика',
+        unique=True,
+        null=True,
+        blank=True,
+    )
+    phone_number = PhoneNumberField(
+        max_length='Телефон поставщика',
+        unique=True,
+        null=True,
+        blank=True,
+    )
+    logo = models.ImageField(
+        verbose_name='Логотип поставщика',
+        upload_to='orders/providers/%Y/%m/%d',
+        null=True,
+        blank=True,
+    )
