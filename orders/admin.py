6d8b9940a12c57dc327c65936500d4b81ef577c0
Anton Akuev <antonakuev945@mail.ru>
2023-12-02 13:25:05 +0300

    feat: создание админ панели для заказа

--- orders/admin.py
+++ orders/admin.py
@@ -4,12 +4,16 @@ from orders.models import orders
 
 @admin.register(orders.Order)
 class OrderAdmin(admin.ModelAdmin):
+    """Модель админа заказа"""
+
     list_display = ('id',)
     readonly_fields = ('quantity', 'sum')
 
 
 @admin.register(orders.OrderProduct)
 class OrderProductAdmin(admin.ModelAdmin):
+    """Модель продукта заказа"""
+
     list_display = ('id',)
     search_fields = ('product__name', 'order__sum')
     readonly_fields = ('date_created',)
