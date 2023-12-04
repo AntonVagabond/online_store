75abadc733982082a18ab26426d4f5a3625bbeec
Anton Akuev <antonakuev945@mail.ru>
2023-12-04 11:29:42 +0300

    feat: добавил в config прогрузку картинок для админки

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,15 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView
+
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
