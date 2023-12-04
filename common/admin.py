fa026b59b2b896d3b6e699ffc0a1d27323976d39
Anton Akuev <antonakuev945@mail.ru>
2023-12-04 12:54:52 +0300

    feat: добавил общюю модель для админки для вывода изображения

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+
+class ModelaAdminWithImage(admin.ModelAdmin):
+    @admin.display(description='Изображение', ordering='image')
+    def image_show(self, obj):
+        if obj.image:
+            return mark_safe(f"<img src='{obj.image.url}' width='60' />")
+        return 'Нет изображения'
