6cb3efb5eb535c6d7f29a31cc6397f4f22b0eec5
Anton Akuev <antonakuev945@mail.ru>
2023-12-04 14:32:02 +0300

    feat: Настроил админ панель

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.admin import TabularInline, StackedInline
+from django.utils.safestring import mark_safe
 
 from products.models import products, categories, providers
+from common.admin import ModelaAdminWithImage
 
 
 # region ----------------------------- INLINE ---------------------------------------
@@ -9,11 +11,7 @@ class ProductFeatureInline(StackedInline):
     """Встраиваемая модель хар-ик для ProductAdmin"""
 
     model = products.ProductFeature
-    fields = (
-        'size',
-        'colour',
-        'patterns',
-    )
+    fields = ('size', 'colour', 'patterns')
 
 
 class ProductDescriptionInline(StackedInline):
@@ -23,55 +21,62 @@ class ProductDescriptionInline(StackedInline):
     fields = ('description',)
 
 
+class SubCategoryInline(TabularInline):
+    """Встраиваемая модель описания для CategoryAdmin"""
+
+    model = categories.Category
+    fields = ('title', 'image', 'description')
+
+
 # endregion -------------------------------------------------------------------------
 
 # region -------------------------- MODEL ADMIN -------------------------------------
 @admin.register(products.Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ModelaAdminWithImage):
     """Модель админа товара"""
 
     list_display = (
         'id',
         'name',
-        'image',
+        'image_show',
         'category',
         'quantity',
         'price',
         'is_available',
     )
-    readonly_fields = (
-        'created_at',
-        'updated_at',
+    fields = (
+        'name',
+        'image',
+        'image_show',
+        'category',
+        'quantity',
+        'price',
+        'is_available',
     )
-    inlines = (
-        ProductFeatureInline,
-        ProductDescriptionInline,
-    )
+    readonly_fields = ('image_show', 'created_at', 'updated_at')
+    inlines = (ProductFeatureInline, ProductDescriptionInline)
 
 
 @admin.register(categories.Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(ModelaAdminWithImage):
     """Модель админа категории"""
 
-    list_display = (
-        'id',
-        'title',
-        'image',
-        'description',
-        'parent_category',
-    )
+    list_display = ('id', 'title', 'image_show', 'description', 'parent_category')
+    fields = ('title', 'image', 'image_show', 'description', 'parent_category')
+    readonly_fields = ('image_show', 'parent_category',)
+    inlines = (SubCategoryInline,)
 
 
 @admin.register(providers.Provider)
 class ProviderAdmin(admin.ModelAdmin):
     """Модель админа поставщика"""
 
-    list_display = (
-        'id',
-        'name',
-        'logo',
-        'email',
-        'phone_number',
-    )
+    list_display = ('id', 'name', 'logo_show', 'email', 'phone_number')
+
+    @admin.display(description='Логотип', ordering='logo')
+    def logo_show(self, obj):
+        if obj.logo:
+            return mark_safe(f"<img src='{obj.logo.url}' width='60' />")
+        return 'Нет логотипа'
 
 # endregion -------------------------------------------------------------------------
