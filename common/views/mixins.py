9eee60299c0de3a0832ac6fe4b74fba90a10608c
Anton Akuev <antonakuev945@mail.ru>
2023-12-06 14:59:54 +0300

    feat: Добавил расширенное представление.

--- common/views/mixins.py
+++ common/views/mixins.py
@@ -2,9 +2,14 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 
-class ListViewSet(GenericViewSet, mixins.ListModelMixin):
+class ExtendedGenericViewSet(GenericViewSet):
+    """Расширенный набор общих представлений"""
+    pass
+
+
+class ListViewSet(ExtendedGenericViewSet, mixins.ListModelMixin):
     """
     Класс включающий базовый набор поведения generic view и включающий
-    модель списка, имеет такие методы как: `get_object`, `get_queryset`, `list`
+    модель списка, имеет такие методы как: `get_object`, `get_queryset`, `list`.
     """
     pass
