54652c4c8ec3f48b54364c1d802054fa63bfc96c
Anton Akuev <antonakuev945@mail.ru>
2023-12-06 10:23:20 +0300

    feat: добавил эндпоинты

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,12 @@ from users.views import users
 
 router = DefaultRouter()
 
+router.register(prefix=r'search', viewset=users.UserListSearchView, basename='users-search')
+
 urlpatterns = [
-    path('users/registration/', users.RegistrationView.as_view(), name='reg'),
+    path('users/registration/', users.RegistrationView.as_view(), name='registration'),
+    path('users/me/', users.MeView.as_view(), name='me'),
+    path('users/change-password/', users.ChangePasswordView.as_view(), name='change_password'),
 ]
 
 urlpatterns += (path('users/', include(router.urls)),)
