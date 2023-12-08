f9e4ce60822df5183d8160f447d2933c78b1dc80
Anton Akuev <antonakuev945@mail.ru>
2023-12-08 14:23:27 +0300

    feat: Сделал сериализатор товара для поиска в списке.

---
+++ products/serializers/api/products.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from products.models.products import Product
+from products.serializers.nested.categories import CategoryNestedSerializer
+from products.serializers.nested.products import ProductImagesNestedSerializer
+
+
+class ProductSearchListSerializer(serializers.ModelSerializer):
+    """Преобразователь поиска товаров"""
+
+    product_images = ProductImagesNestedSerializer()
+    category = CategoryNestedSerializer()
+
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'product_images',
+            'category',
+        )
