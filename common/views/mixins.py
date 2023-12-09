49458ffbac633902309279fd721b7d6e4d18e10c
Anton Akuev <antonakuev945@mail.ru>
2023-12-09 16:40:25 +0300

    feat: Добавил класс миксин для CRUD-операций.

--- common/views/mixins.py
+++ common/views/mixins.py
@@ -10,6 +10,21 @@ class ExtendedGenericViewSet(GenericViewSet):
 class ListViewSet(ExtendedGenericViewSet, mixins.ListModelMixin):
     """
     Класс включающий базовый набор поведения generic view и включающий
-    модель списка, имеет такие методы как: `get_object`, `get_queryset`, `list`.
+    модель списка, имеет такие методы как: `get_object`, `get_queryset`, `list`
     """
+    pass
+
+
+class CRUListViewSet(ExtendedGenericViewSet,
+                     mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.ListModelMixin):
+    """Класс включающий в себя базовые операции, кроме DestroyModelMixin"""
+    pass
+
+
+class CRUDListViewSet(CRUListViewSet,
+                      mixins.DestroyModelMixin):
+    """Класс включающий в себя CRUD-операции"""
     pass
