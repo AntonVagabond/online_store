80b47cc4a7dbfb03f336e24bed4b1c982c329f3c
Anton Akuev <antonakuev945@mail.ru>
2023-12-09 13:21:15 +0300

    feat: Добавил вложенный сериализатор для поставщика.

---
+++ products/serializers/nested/providers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from products.models.providers import Provider
+
+
+class ProviderNestedSerializer(serializers.ModelSerializer):
+    """Вложенный преобразователь поставщика товара"""
+
+    class Meta:
+        model = Provider
+        fields = ('name', 'logo')
