392fd92d9832742b7b738399b295755d0af137d5
Anton Akuev <antonakuev945@mail.ru>
2023-12-09 13:44:38 +0300

    feat: Создал CRU для товаров.

--- products/serializers/api/products.py
+++ products/serializers/api/products.py
@@ -2,15 +2,90 @@ from rest_framework import serializers
 
 from products.models.products import Product
 from products.serializers.nested.categories import CategoryNestedSerializer
-from products.serializers.nested.products import ProductImagesNestedSerializer
+from products.serializers.nested import products
+from products.serializers.nested.providers import ProviderNestedSerializer
 
 
 class ProductSearchSerializer(serializers.ModelSerializer):
     """Преобразователь поиска товаров"""
 
-    product_images = ProductImagesNestedSerializer()
+    product_images = products.ProductImagesNestedSerializer()
     category = CategoryNestedSerializer()
 
     class Meta:
         model = Product
-        fields = ('id', 'name', 'product_images', 'category')
+        fields = ('id', 'name', 'category', 'product_images')
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    """Преобразователь списка товаров"""
+
+    product_images = products.ProductImagesNestedSerializer()
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'price', 'quantity', 'product_images')
+
+
+class ProductRetrieveSerializer(serializers.ModelSerializer):
+    """Преобразователь извлечения товара"""
+
+    category = CategoryNestedSerializer()
+    provider = ProviderNestedSerializer()
+    product_description = products.ProductDescriptionNestedSerializer()
+    product_feature = products.ProductFeatureNestedSerializer()
+    product_images = products.ProductImagesNestedSerializer()
+
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'is_available',
+            'price',
+            'quantity',
+            'category',
+            'provider',
+            'product_description',
+            'product_feature',
+            'product_images',
+        )
+
+
+class ProductCreateSerializer(serializers.ModelSerializer):
+    """Преобразователь создания товара"""
+
+    product_description = products.ProductDescriptionNestedSerializer()
+    product_feature = products.ProductFeatureNestedSerializer()
+    product_images = products.ProductImagesNestedSerializer()
+
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'is_available',
+            'price',
+            'quantity',
+            'product_description',
+            'product_feature',
+            'product_images',
+        )
+
+
+class ProductUpdateSerializer(serializers.ModelSerializer):
+    """Преобразователь обновления товара"""
+
+    product_description = products.ProductDescriptionNestedSerializer()
+    product_feature = products.ProductFeatureNestedSerializer()
+    product_images = products.ProductImagesNestedSerializer()
+
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'is_available',
+            'price',
+            'quantity',
+            'product_description',
+            'product_feature',
+            'product_images',
+        )
