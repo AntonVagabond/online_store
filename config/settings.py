385ef76d22341a3ffb31c65830b9967337645b87
Anton Akuev <antonakuev945@mail.ru>
2023-12-12 16:13:37 +0300

    feat: Добавил jwt раздел black list

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # packages
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'django_filters',
     'corsheaders',
     'djoser',
