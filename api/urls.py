2865895b855e582bf6573d3f42960cc4fe0f4a5f
Anton Akuev <antonakuev945@mail.ru>
2023-12-12 08:54:33 +0300

    feat: Добавил путь для токенов.

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,18 @@ from django.urls import path, include
 from api.spectacular.urls import urlpatterns as doc_urls
 from users.urls import urlpatterns as user_urls
 from products.urls import urlpatterns as product_urls
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView,
+                                            TokenVerifyView)
+
 
 app_name = 'api'
 
 urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+    path('token/verify/', TokenVerifyView.as_view()),
 ]
 
 urlpatterns += doc_urls
