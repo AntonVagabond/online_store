8480f4992ea6e397fa0116a7ae9494e11c34913d
Anton Akuev <antonakuev945@mail.ru>
2023-12-12 13:30:52 +0300

    feat: Добавил настройку с автотокеном.

--- config/settings.py
+++ config/settings.py
@@ -184,7 +184,7 @@ DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': '#/activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
+    'SEND_ACTIVATION_EMAIL': False,
     'SERIALIZERS': {},
 }
 
