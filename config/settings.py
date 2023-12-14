0fcb7c82ab6de94a7cb0be2be7ac84becaecf68a
Anton Akuev <antonakuev945@mail.ru>
2023-12-14 09:57:09 +0300

    feat: поменял в djoser наимен файла.

--- config/settings.py
+++ config/settings.py
@@ -202,7 +202,7 @@ DJOSER = {
     'SEND_CONFIRMATION_EMAIL': True,
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'ACTIVATION_URL': '/activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS': {
         'user_create': 'users.serializers.api.auth.RegistrationSerializer',
