bc0f403c36c40cb81eb902218200b8e52ec40f81
Anton Akuev <antonakuev945@mail.ru>
2023-12-20 14:45:50 +0300

    feat: Создание рабочей корзины.

--- carts/views/carts.py
+++ carts/views/carts.py
@@ -1,32 +1,47 @@
-from crum import get_current_user
 from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import permissions
 
-from carts.models.carts import Cart, CartItem
+from carts.models.carts import Cart
 from carts.serializers.api import carts as carts_s
 from common.views import mixins
 
 
 @extend_schema_view(
+    list=extend_schema(
+        summary='Получить информацию о корзине покупок',
+        tags=['Корзина'],
+    ),
     create=extend_schema(
         summary='Добавить товар в корзину',
         tags=['Корзина'],
     ),
+    partial_update=extend_schema(
+        summary='Частичное обновление товара в корзине',
+        tags=['Корзина'],
+    ),
+    destroy=extend_schema(
+        summary='Удалить записи о покупках',
+        tags=['Корзина'],
+    )
 )
-class CartItemViewSet(mixins.CreateViewSet):
+class CartViewSet(mixins.CUDListViewSet):
     """Представление корзины."""
-    permission_classes = (permissions.AllowAny,)
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     queryset = Cart.objects.all()
-    serializer_class = carts_s.CartToAddSerializer
+    serializer_class = carts_s.CartSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    multi_serializer_class = {
+        'list': carts_s.CartDetailSerializer,
+        'create': carts_s.CartSerializer,
+        'partial_update': carts_s.CartUpdateSerializer,
+    }
 
-        user = get_current_user()
-        session = request.session.session_key
-        cart = Cart.objects.create(user=user, session=session)
+    http_method_names = ('get', 'post', 'patch', 'delete')
+    lookup_field = 'products_id'
 
-        return super().create(request, *args, **kwargs)
+    # def get_queryset(self):
+    #     user = get_current_user()
+    #     return Cart.objects.filter(user=user)
 
