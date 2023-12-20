55f8229e80aa09c691deb47fa180094ba02f63e7
Anton Akuev <antonakuev945@mail.ru>
2023-12-20 17:14:00 +0300

    feat: Создал стандартную админ панель.

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from carts.models import carts
 
-# Register your models here.
+
+# region ----------------------------- MODEL ADMIN ----------------------------------
+@admin.register(carts.Cart)
+class CartAdmin(admin.ModelAdmin):
+    """
+    Модель админа корзины.
+
+    Аттрибуты:
+        * `list_display` (tuple[str]): отображение списка.
+        * `list_display_links` (tuple[str]): список отображаемых ссылок.
+        * `fields` (tuple[str]): поля.
+        * `inlines` (tuple[inlines]): встроенные.
+    """
+    list_display = ('id', 'user', 'products', 'quantity')
+    list_display_links = ('id', 'user')
+    fields = ('user', 'products', 'quantity')
+# endregion -------------------------------------------------------------------------
