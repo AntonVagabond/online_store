a28396d771bc19859954c024c479c9d25ca769a1
Anton Akuev <antonakuev945@mail.ru>
2023-12-22 14:06:33 +0300

    feat: Создание рабочего показа и создания корзины.

--- carts/serializers/api/carts.py
+++ carts/serializers/api/carts.py
@@ -3,39 +3,104 @@ from typing import Union
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ParseError
 
-from carts.models.carts import Cart
+from carts.models.carts import Cart, CartItem
 from carts.serializers.nested.carts import CartProductNestedSerializer
+from carts.services.carts import CartService, CartItemService
 from products.models.products import Product
 
 User = get_user_model()
 
 
-class CartDetailSerializer(serializers.ModelSerializer):
-    """
-    Преобразователь корзины.
+# class CartDetailSerializer(serializers.ModelSerializer):
+#     """
+#     Преобразователь корзины.
+#
+#     Аттрибуты:
+#         * `products` (CartProductNestedSerializer): товары корзины.
+#     """
+#
+#     products = CartProductNestedSerializer(read_only=True)
+#
+#     class Meta:
+#         model = Cart
+#         fields = ('products', 'quantity')
+#
+#
+# class CartSerializer(serializers.Serializer):
+#     """
+#     Преобразователь создания корзины.
+#
+#     Аттрибуты:
+#         * `user` (HiddenField): пользователь.
+#         * `quantity` (IntegerField): products.
+#         * `products` (PrimaryKeyRelatedField): products.
+#     """
+#     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+#     quantity = serializers.IntegerField(
+#         required=True,
+#         label='Количество',
+#         min_value=1,
+#         error_messages={
+#             'min_value': 'Количество товаров не может быть меньше одного',
+#             'required': 'Пожалуйста, выберите количество покупок',
+#         },
+#     )
+#     products = serializers.PrimaryKeyRelatedField(
+#         required=True,
+#         queryset=Product.objects.all(),
+#     )
+#
+#     def create(self, validated_data: dict[str, Union[Product, int, User]]) -> Cart:
+#         """Создание корзины и проверка на её наличие."""
+#         user = self.context['request'].user
+#         products = validated_data['products']
+#         quantity = validated_data['quantity']
+#
+#         cart = Cart.objects.filter(user=user, products=products)
+#
+#         # Проверка на создание записи.
+#         if cart:
+#             cart = cart[0]
+#             cart.quantity += quantity
+#             cart.save()
+#         else:
+#             with transaction.atomic():
+#                 cart = Cart.objects.create(**validated_data)
+#         return cart
+#
+#
+# class CartUpdateSerializer(serializers.ModelSerializer):
+#     """
+#     Преобразователь обновления товара в корзине.
+#
+#     Аттрибуты:
+#         * `user` (HiddenField): пользователь.
+#         * `quantity` (IntegerField): products.
+#     """
+#     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+#     quantity = serializers.IntegerField(
+#         required=True,
+#         label='Количество',
+#         min_value=1,
+#         error_messages={
+#             'min_value': 'Количество товаров не может быть меньше одного',
+#             'required': 'Пожалуйста, выберите количество покупок',
+#         },
+#     )
+#
+#     class Meta:
+#         model = Cart
+#         fields = ('user', 'quantity')
 
-    Аттрибуты:
-        * `products` (CartProductNestedSerializer): товары корзины.
-    """
+# -----------------------------------------------------------------------------------
 
-    products = CartProductNestedSerializer(read_only=True)
-
-    class Meta:
-        model = Cart
-        fields = ('products', 'quantity')
-
-
-class CartSerializer(serializers.Serializer):
-    """
-    Преобразователь создания корзины.
-
-    Аттрибуты:
-        * `user` (HiddenField): пользователь.
-        * `quantity` (IntegerField): products.
-        * `products` (PrimaryKeyRelatedField): products.
-    """
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+class CartItemSerializer(serializers.Serializer):
+    product = serializers.PrimaryKeyRelatedField(
+        required=True,
+        queryset=Product.objects.all(),
+    )
     quantity = serializers.IntegerField(
         required=True,
         label='Количество',
@@ -45,31 +110,21 @@ class CartSerializer(serializers.Serializer):
             'required': 'Пожалуйста, выберите количество покупок',
         },
     )
-    products = serializers.PrimaryKeyRelatedField(
-        required=True,
-        queryset=Product.objects.all(),
-    )
 
-    def create(self, validated_data: dict[str, Union[Product, int, User]]) -> Cart:
-        """Создание корзины и проверка на её наличие."""
-        user = self.context['request'].user
-        products = validated_data['products']
-        quantity = validated_data['quantity']
-
-        cart = Cart.objects.filter(user=user, products=products)
-
-        # Проверка на создание записи.
-        if cart:
-            cart = cart[0]
-            cart.quantity += quantity
-            cart.save()
-        else:
-            with transaction.atomic():
-                cart = Cart.objects.create(**validated_data)
-        return cart
+    def create(
+            self,
+            validated_data: dict[str, Union[Product, int]],
+    ) -> Union[ParseError, CartItem]:
+        """Создание содержимого корзины и проверка на её наличие."""
+        cart = CartService.get_or_create_cart(self.context['request'])
+        cart_item_init = CartItemService(cart=cart, validated_data=validated_data)
+        cart_item = cart_item_init.create_cart_item()
+        if cart_item is None:
+            raise ParseError('Этот товар уже добавлен в корзину!')
+        return cart_item
 
 
-class CartUpdateSerializer(serializers.ModelSerializer):
+class CartItemUpdateSerializer(serializers.ModelSerializer):
     """
     Преобразователь обновления товара в корзине.
 
@@ -77,7 +132,6 @@ class CartUpdateSerializer(serializers.ModelSerializer):
         * `user` (HiddenField): пользователь.
         * `quantity` (IntegerField): products.
     """
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     quantity = serializers.IntegerField(
         required=True,
         label='Количество',
@@ -90,4 +144,13 @@ class CartUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Cart
-        fields = ('user', 'quantity')
+        fields = ('quantity',)
+
+
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    products = CartProductNestedSerializer(many=True)
+
+    class Meta:
+        model = Cart
+        fields = ('user', 'products')
--- carts/views/carts.py
+++ carts/views/carts.py
@@ -6,6 +6,41 @@ from carts.serializers.api import carts as carts_s
 from common.views import mixins
 
 
+# @extend_schema_view(
+#     list=extend_schema(
+#         summary='Получить информацию о корзине покупок',
+#         tags=['Корзина'],
+#     ),
+#     create=extend_schema(
+#         summary='Добавить товар в корзину',
+#         tags=['Корзина'],
+#     ),
+#     partial_update=extend_schema(
+#         summary='Частичное обновление товара в корзине',
+#         tags=['Корзина'],
+#     ),
+#     destroy=extend_schema(
+#         summary='Удалить записи о покупках',
+#         tags=['Корзина'],
+#     )
+# )
+# class CartViewSet(mixins.CUDListViewSet):
+#     """Представление корзины."""
+#
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+#
+#     queryset = Cart.objects.all()
+#     serializer_class = carts_s.CartSerializer
+#
+#     multi_serializer_class = {
+#         'list': carts_s.CartDetailSerializer,
+#         'create': carts_s.CartSerializer,
+#         'partial_update': carts_s.CartUpdateSerializer,
+#     }
+#
+#     http_method_names = ('get', 'post', 'patch', 'delete')
+#     lookup_field = 'products_id'
+
 @extend_schema_view(
     list=extend_schema(
         summary='Получить информацию о корзине покупок',
@@ -15,14 +50,14 @@ from common.views import mixins
         summary='Добавить товар в корзину',
         tags=['Корзина'],
     ),
-    partial_update=extend_schema(
-        summary='Частичное обновление товара в корзине',
-        tags=['Корзина'],
-    ),
-    destroy=extend_schema(
-        summary='Удалить записи о покупках',
-        tags=['Корзина'],
-    )
+    # partial_update=extend_schema(
+    #     summary='Частичное обновление товара в корзине',
+    #     tags=['Корзина'],
+    # ),
+    # destroy=extend_schema(
+    #     summary='Удалить записи о покупках',
+    #     tags=['Корзина'],
+    # )
 )
 class CartViewSet(mixins.CUDListViewSet):
     """Представление корзины."""
@@ -33,9 +68,9 @@ class CartViewSet(mixins.CUDListViewSet):
     serializer_class = carts_s.CartSerializer
 
     multi_serializer_class = {
-        'list': carts_s.CartDetailSerializer,
-        'create': carts_s.CartSerializer,
-        'partial_update': carts_s.CartUpdateSerializer,
+        'list': carts_s.CartSerializer,
+        'create': carts_s.CartItemSerializer,
+        # 'partial_update': carts_s.CartItemUpdateSerializer,
     }
 
     http_method_names = ('get', 'post', 'patch', 'delete')
