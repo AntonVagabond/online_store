9fca6de6886d73fdaf33bcf302b618fb320f1aa5
Anton Akuev <antonakuev945@mail.ru>
2023-12-29 19:32:15 +0300

    feat: Создал преобраз-тели для смен, пароля сброс пароля и т.д.

--- users/serializers/api/users.py
+++ users/serializers/api/users.py
@@ -1,8 +1,11 @@
 from typing import Optional
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ParseError
+
 from djoser import serializers as djoser_serializers
 
 from users.models.profile import Profile
@@ -14,7 +17,44 @@ from users.serializers.nested.profile import (
 User = get_user_model()
 
 
-class MeSerializer(djoser_serializers.UserCreateSerializer):
+# region -------------------- AUTHORISATION AND REGISTRATION ------------------------
+class RegistrationSerializer(djoser_serializers.UserCreateSerializer):
+    """
+    Преобразователь регистрации пользователей.
+
+    Аттрибуты:
+        * `email` (EmailField): почта.
+        * `password` (CharField): пароль.
+    """
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True,
+    )
+
+    class Meta(djoser_serializers.UserCreateSerializer.Meta):
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+    @staticmethod
+    def validate_email(value: str) -> str:
+        """Проверка на уникальность почты."""
+
+        email = value.lower()
+        if User.objects.filter(email=email).exists():
+            raise ParseError('Пользователь с такой почтой уже зарегистрирован!')
+        return email
+
+
+class CustomActivationSerializer(djoser_serializers.ActivationSerializer):
+    """Преобразователь для активации пользователя"""
+    pass
+# endregion -------------------------------------------------------------------------
+
+
+# region --------------------------------- USER -------------------------------------
+class MeSerializer(serializers.ModelSerializer):
     """
     Преобразователь пользователя.
 
@@ -24,7 +64,7 @@ class MeSerializer(djoser_serializers.UserCreateSerializer):
 
     profile = ProfileShortSerializer()
 
-    class Meta(djoser_serializers.UserCreateSerializer.Meta):
+    class Meta:
         model = User
         fields = (
             'id',
@@ -100,3 +140,55 @@ class UserListSearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'full_name')
+
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    """
+    Преобразователь смены пароля.
+
+    Аттрибуты:
+        * `old_password` (CharField): старый пароль.
+        * `new_password` (CharField): новый пароль.
+    """
+
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password')
+
+    def validate(self, attrs: dict[str, str]) -> dict[str, str]:
+        """Проверка на корректность пароля."""
+        user = self.instance
+        old_password = attrs.pop('old_password')
+        if not user.check_password(raw_password=old_password):
+            raise ParseError('Проверьте правильность текущего пароля!')
+        return attrs
+
+    @staticmethod
+    def validate_new_password(password: str) -> str:
+        """Проверка на корректность нового пароля."""
+        validate_password(password=password)
+        return password
+
+    def update(self, instance: User, validated_data: dict[str, str]) -> User:
+        """Обновление пароля в модели User."""
+        password = validated_data.pop('new_password')
+        # Хэшируем пароль
+        instance.set_password(raw_password=password)
+        instance.save()
+        return instance
+
+
+class PasswordResetSerializer(djoser_serializers.SendEmailResetSerializer):
+    """Преобразователь для запроса о новом пароле на почту."""
+    pass
+
+
+class CustomPasswordResetConfirmSerializer(
+    djoser_serializers.PasswordResetConfirmSerializer
+):
+    """Преобразователь для сброса пароля"""
+    pass
+# endregion -------------------------------------------------------------------------
