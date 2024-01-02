f8fb4a759ce87cdb9721f2649843e7b95090e18c
Anton Akuev <antonakuev945@mail.ru>
2024-01-02 10:57:28 +0300

    feat: Создал файл и написал классы для аутентификации и добавил для них end-point.

--- users/urls.py
+++ users/urls.py
@@ -3,13 +3,18 @@ from rest_framework.routers import DefaultRouter
 
 from users.utils import is_route_selected
 from users.views import users
+from users.views import auth
 
 router = DefaultRouter()
 
 router.register(prefix='', viewset=users.CustomUserViewSet, basename='users')
 router.register(prefix=r'search', viewset=users.UserListSearchView, basename='users-search')
 
-urlpatterns = []
+urlpatterns = [
+    path('auth/jwt/create/', auth.CustomTokenObtainPairView.as_view(), name='jwt-create'),
+    path('auth/jwt/refresh/', auth.CustomTokenRefreshView.as_view(), name='jwt-refresh'),
+    path('auth/jwt/verify/', auth.CustomTokenVerifyView.as_view(), name='jwt-verify'),
+]
 
 # Проверка на разрешенные конечные точки.
 selected_user_routes = list(filter(is_route_selected, router.urls))
