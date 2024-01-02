f7b063045a07325c64f25932955cea8026b1ab12
Anton Akuev <antonakuev945@mail.ru>
2024-01-02 10:05:28 +0300

    feat: Добавил проверку для endpoint-ов, которые должны отображаться на схеме.

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from users.utils import is_route_selected
 from users.views import users
-from users.views import auth
 
 router = DefaultRouter()
 
+router.register(prefix='', viewset=users.CustomUserViewSet, basename='users')
 router.register(prefix=r'search', viewset=users.UserListSearchView, basename='users-search')
 
-urlpatterns = [
-    path('users/registration/', auth.RegistrationView.as_view(), name='registration'),
-    path('users/change-password/', auth.ChangePasswordView.as_view(), name='change_password'),
-    path('users/me/', users.MeView.as_view(), name='me'),
-    path('users/me/edit/', users.MeUpdateView.as_view(), name='me_edit'),
-]
+urlpatterns = []
 
-urlpatterns += (path('users/', include(router.urls)),)
+# Проверка на разрешенные конечные точки.
+selected_user_routes = list(filter(is_route_selected, router.urls))
+
+urlpatterns += (path('users/', include(selected_user_routes)),)
