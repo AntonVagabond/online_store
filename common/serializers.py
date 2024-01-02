3ece5e35e5c9eda1ced6e9cb42e223b7137f2bb8
menma331 <romaxid@yandex.by>
2024-01-02 13:48:55 +0300

    Добавлен базовый класс сериализатора для категории

---
+++ common/serializers.py
@@ -0,0 +1,19 @@
+from typing import OrderedDict, Union
+
+from rest_framework import serializers
+
+from products.models.categories import Category
+
+
+class BaseCategorySerializer(serializers.ModelSerializer):
+    """Базовый класс сериализатора для категории"""
+    class Meta:
+        abstract = True
+
+    def to_representation(
+                          self, obj: Category
+    ) -> OrderedDict[str, Union[int, str, None]]:
+        # Добавляем поле 'children' и его полное значение.
+        self.fields['children'] = self.__class__(many=True)
+        # Повторяется этот процесс, пока есть вложенность.
+        return super().to_representation(obj)
--- products/serializers/api/categories.py
+++ products/serializers/api/categories.py
@@ -1,56 +1,29 @@
-from typing import OrderedDict, Union
-
 from rest_framework import serializers
 
+from common.serializers import BaseCategorySerializer
 from products.models.categories import Category
 
 
-class CategorySearchSerializer(serializers.ModelSerializer):
+class CategorySearchSerializer(BaseCategorySerializer):
     """Преобразователь поиска категорий."""
 
     class Meta:
         model = Category
         fields = ('id', 'title', 'children')
 
-    def to_representation(
-            self, obj: Category
-    ) -> OrderedDict[str, Union[int, str, None]]:
-        # Добавляем поле 'children' и его полное значение.
-        self.fields['children'] = CategorySearchSerializer(many=True)
-        # Повторяется этот процесс, пока есть еще вложенность.
-        return super(CategorySearchSerializer, self).to_representation(obj)
 
-
-class CategoryListSerializer(serializers.ModelSerializer):
+class CategoryListSerializer(BaseCategorySerializer):
     """Преобразователь списка категорий."""
 
     class Meta:
         model = Category
         fields = ('id', 'title', 'image', 'children')
 
-    def to_representation(
-            self, obj: Category
-    ) -> OrderedDict[str, Union[int, str, None]]:
-        # Добавляем поле 'children' и его полное значение.
-        self.fields['children'] = CategoryListSerializer(many=True)
-        # Повторяется этот процесс, пока есть еще вложенность.
-        return super(CategoryListSerializer, self).to_representation(obj)
 
-
-class CategoryRetrieveSerializer(serializers.ModelSerializer):
-    """Преобразователь извлечения категории."""
-
+class CategoryRetrieveSerializer(BaseCategorySerializer):
     class Meta:
         model = Category
         fields = ('id', 'title', 'image', 'description', 'children')
-
-    def to_representation(
-            self, obj: Category
-    ) -> OrderedDict[str, Union[int, str, None]]:
-        # Добавляем поле 'children' и его полное значение.
-        self.fields['children'] = CategoryRetrieveSerializer(many=True)
-        # Повторяется этот процесс, пока есть еще вложенность.
-        return super(CategoryRetrieveSerializer, self).to_representation(obj)
 
 
 class CategoryCreateSerializer(serializers.ModelSerializer):
