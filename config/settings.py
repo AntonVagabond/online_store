9ca45b929754c1f954738d2a43fbda995984d9d2
Anton Akuev <antonakuev945@mail.ru>
2024-01-05 19:57:12 +0300

    feat: Добавление Celery и Redis.

--- config/settings.py
+++ config/settings.py
@@ -211,7 +211,7 @@ SIMPLE_JWT = {
     'USER_ID_CLAIM': 'user_id',
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'tken_type',
+    'TOKEN_TYPE_CLAIM': 'token_type',
 
     'JTI_CLAIM': 'jti',
 
