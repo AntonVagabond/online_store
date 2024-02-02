73e05b97203c7a86af0268c4dd5bf62ee38037b4
Anton Akuev <antonakuev945@mail.ru>
2024-02-02 23:35:45 +0300

    fix: Удалил OrderCreateService.

--- carts/services/orders.py
+++ carts/services/orders.py
@@ -102,32 +102,8 @@ class AddItemToOrderService:
 
     def execute(self) -> None:
         """Выполнить добавление товара в заказ."""
-        with transaction.atomic():
-            cart = self._get_cart_current_user()
-            cart_items = self._get_item_from_this_cart(cart)
-            self._add_product_an_order_items(cart_items)
-            self._update_count_of_products()
-            self._delete_cart(cart)
-
-
-class OrderCreateService:
-    """Сервисная часть для создания заказа."""
-
-    def __init__(self, order: Order, delivery_data: dict[str]) -> None:
-        self._order = order
-        self._delivery_data = delivery_data
-
-    def _adding_order_creation_time(self) -> None:
-        """Добавление времени у создания заказа."""
-        date = self._order.order_date
-        additional_data = {key: date for key in ('created_at', 'update_at')}
-        self._delivery_data |= additional_data
-
-    def _create_delivery(self) -> None:
-        """Создание доставки."""
-        Delivery.objects.create(order_id=self._order.pk, **self._delivery_data)
-
-    def execute(self) -> None:
-        """Выполнить создание заказа, доставки и вернуть экземпляр заказа."""
-        self._adding_order_creation_time()
-        self._create_delivery()
+        cart = self._get_cart_current_user()
+        cart_items = self._get_item_from_this_cart(cart)
+        self._add_product_an_order_items(cart_items)
+        self._update_count_of_products()
+        self._delete_cart(cart)
