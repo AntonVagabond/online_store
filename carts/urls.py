4bebf1b794a7bd92e5cbef2c496ef7a2d55afefd
Anton Akuev <antonakuev945@mail.ru>
2024-02-10 14:38:56 +0300

    fix: Убрал эндпоинты.

--- carts/urls.py
+++ carts/urls.py
@@ -2,17 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from carts.views import carts
-from carts.views import orders
-from carts.views import couriers
 
 router = DefaultRouter()
 
 router.register(prefix='cart/item', viewset=carts.CartItemViewSet, basename='cart_item')
 router.register(prefix='cart', viewset=carts.CartViewSet, basename='cart')
-router.register(prefix='orders', viewset=orders.OrderMakingViewSet, basename='orders')
-router.register(prefix='order_details', viewset=orders.OrderDetailViewSet, basename='order_details')
-router.register(prefix='order_list', viewset=orders.UserOrdersViewSet, basename='order_list')
-router.register(prefix='couriers', viewset=couriers.CourierViewSet, basename='couriers')
+
 
 urlpatterns = []
 
