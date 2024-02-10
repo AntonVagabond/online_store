76edc52e9395e2c242001ef2b6ac39c052254491
Anton Akuev <antonakuev945@mail.ru>
2024-02-10 14:21:56 +0300

    feat: Добавил url доставки и заказа.

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from api.spectacular.urls import urlpatterns as doc_urls
 from users.urls import urlpatterns as user_urls
 from products.urls import urlpatterns as product_urls
 from carts.urls import urlpatterns as cart_urls
+from delivers.urls import urlpatterns as deliver_urls
+from orders.urls import urlpatterns as order_urls
 
 
 app_name = 'api'
@@ -12,3 +14,5 @@ urlpatterns += doc_urls
 urlpatterns += user_urls
 urlpatterns += product_urls
 urlpatterns += cart_urls
+urlpatterns += deliver_urls
+urlpatterns += order_urls
