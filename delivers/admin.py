c9e6d19bb407045f31d8d498e51382cd0aad83b9
Anton Akuev <antonakuev945@mail.ru>
2024-02-10 15:33:29 +0300

    feat: Перенес классы админки.

--- delivers/admin.py
+++ delivers/admin.py
@@ -1,3 +1,109 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import couriers, delivers
+
+
+# region ----------------------------- MODEL ADMIN ----------------------------------
+@admin.register(couriers.Courier)
+class CourierAdmin(admin.ModelAdmin):
+    """
+    Модель админа курьера.
+
+    Атрибуты:
+        * `list_display` (tuple[str]): отображаемые поля в списке.
+        * `list_display_links` (tuple[str]): список отображаемых ссылок.
+        * 'list_per_page' (int): количество отображаемых курьеров на странице.
+        * 'ordering' (tuple[str]): сортировка по id.
+        * `fields` (tuple[str]): поля.
+    """
+    list_display = (
+        'id',
+        'name',
+        'is_available'
+    )
+    list_display_links = ('id', 'name')
+    list_per_page = 10
+    ordering = ('id',)
+    fields = (
+        'name',
+        'phone_number',
+        'email',
+        'address',
+        'vehicle',
+        'is_available',
+        'delivery_cost'
+    )
+    filter_horizontal = ('vehicle',)
+
+
+@admin.register(couriers.Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    """
+    Модель админа транспорта.
+
+    Атрибуты:
+        * `list_display` (tuple[str]): отображаемые поля в списке.
+        * 'list_per_page' (int): количество отображаемых курьеров на странице.
+        * 'ordering' (tuple[str]): сортировка по id.
+        * `fields` (tuple[str]): поля.
+    """
+    list_display = ('name',)
+    list_per_page = 10
+    ordering = ('name',)
+    fields = ('name',)
+
+
+@admin.register(delivers.Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    """
+    Модель админа Доставки.
+
+    Атрибуты:
+        * `list_display` (tuple[str]): отображаемые поля в списке.
+        * 'list_per_page' (int): количество отображаемых доставок на странице.
+        * `fields` (tuple[str]): поля.
+        * 'readonly_fields' (tuple[str]): Поля только для чтения.
+        * 'ordering' (tuple[str]): сортировка по последнему id.
+    """
+    list_display = (
+        'id',
+        'order',
+        'delivery_method',
+        'delivery_status',
+        'courier'
+    )
+    list_per_page = 10
+    fields = (
+        'order',
+        'delivery_method',
+        'delivery_status',
+        'created_at',
+        'update_at',
+        'courier',
+        'notes',
+    )
+    readonly_fields = (
+        'created_at',
+        'update_at',
+    )
+    ordering = ('-id',)
+
+
+@admin.register(delivers.DeliveryStatus)
+class DeliveryStatusAdmin(admin.ModelAdmin):
+    """
+        Модель админа Статуса доставки.
+
+        Атрибуты:
+            * `fields` (tuple[str]): поля.
+            * `list_display` (tuple[str]): отображаемые поля в списке.
+    """
+    fields = (
+        'name',
+        'description',
+    )
+    list_display = (
+        'id',
+        'name'
+    )
+# endregion -------------------------------------------------------------------------
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import orders
+
+
+# region ------------------------------- INLINE -------------------------------------
+class OrderItemInline(admin.TabularInline):
+    """
+    Встраиваемая модель содержимого товара для CartAdmin.
+
+    Аттрибуты:
+        * `model` (OrderItem): модель для изображений товара.
+        * `fields` (tuple[str]): поля.
+        * `readonly_fields` (tuple[str]): поля для чтения.
+    """
+    model = orders.OrderItem
+    fields = ('id', 'product', 'quantity')
+# endregion -------------------------------------------------------------------------
+
+
+# region ----------------------------- MODEL ADMIN ----------------------------------
+@admin.register(orders.Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Модель админа заказа.
+
+    Атрибуты:
+        * `list_display` (tuple[str]): отображаемые поля в списке.
+        * `list_display_links` (tuple[str]): список отображаемых ссылок.
+        * 'list_per_page' (int): количество отображаемых заказов на странице.
+        * `fields` (tuple[str]): поля.
+        * `readonly_fields` (tuple[str]): поля только для чтения.
+        * `inlines` (tuple[inlines]): встроенные.
+    """
+    list_display = (
+        'id',
+        'user',
+        'order_status',
+        'sequence_number',
+        'order_date',
+    )
+    list_display_links = ('id', 'user')
+    list_per_page = 10
+    ordering = ('-id',)
+    fields = (
+        'user',
+        'order_status',
+        'sequence_number',
+        'transaction_number',
+        'post_script',
+        'order_amount',
+        'address',
+        'signer_mobile',
+        'order_date',
+    )
+    readonly_fields = (
+        'sequence_number',
+        'transaction_number',
+        'order_date',
+    )
+    inlines = (OrderItemInline,)
+# endregion -------------------------------------------------------------------------
