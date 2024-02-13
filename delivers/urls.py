44e5fb3b1da14ddab7e6cf473cf8aa7a01789da6
menma331 <romaxid@yandex.by>
2024-02-13 18:33:33 +0300

    feat: зарегистрирован url к delivery schema.

--- delivers/urls.py
+++ delivers/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import couriers
+from .views import delivery
 
 router = DefaultRouter()
 
 router.register(prefix='couriers', viewset=couriers.CourierViewSet, basename='couriers')
+router.register(prefix='delivery', viewset=delivery.DeliveryViewSet, basename='delivery')
 
 urlpatterns = []
 
