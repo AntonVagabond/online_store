cd8c770b8a8b74875886223be90918cc21c523a4
Anton Akuev <antonakuev945@mail.ru>
2024-02-25 21:17:04 +0300

    feat: Добавил расширенные атрибуты аутентификации и разрешения.

--- products/views/products.py
+++ products/views/products.py
@@ -1,17 +1,18 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema_view, extend_schema
-from rest_framework import permissions
+from rest_framework import authentication
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework_simplejwt import authentication as jwt_authentication
 
 from common.views.mixins import CRUDListViewSet
 from products.models.products import Product
+from products.permissions import products as permissions_prod
 from products.serializers.api import products as products_s
 
 
-# TODO: добавить валидацию для клиента который создает, обновляет, удаляет записи.
 @extend_schema_view(
     # region ----------------------- РАСШИРЕННАЯ СХЕМА ------------------------------
     list=extend_schema(
@@ -43,30 +44,33 @@ from products.serializers.api import products as products_s
 )
 class ProductView(CRUDListViewSet):
     """Представление товара."""
-
-    multi_permission_classes = {
-        'retrieve': (permissions.AllowAny,),
-        'create': (permissions.IsAdminUser,),
-        'partial_update': (permissions.IsAdminUser,),
-        'destroy': (permissions.IsAdminUser,),
-        'search': (permissions.AllowAny,),
-    }
-
     queryset = Product.objects.all().select_related(
         'category',
         'provider',
         'product_description',
         'product_feature'
     )
-    serializer_class = products_s.ProductListSerializer
 
+    authentication_classes = jwt_authentication.JWTAuthentication
+    multi_authentication_classes = {
+        'list': (authentication.BasicAuthentication,),
+        'retrieve': (authentication.BasicAuthentication,),
+        'search': (authentication.BasicAuthentication,)
+    }
+
+    permission_classes = (permissions_prod.IsProviderOrStaffOrReadOnly,)
+    multi_permission_classes = {
+        'partial_update': (permissions_prod.IsProductProviderOrStaff,),
+        'destroy': (permissions_prod.IsProductProviderOrStaff,),
+    }
+
+    serializer_class = products_s.ProductListSerializer
     multi_serializer_class = {
         'list': products_s.ProductListSerializer,
         'retrieve': products_s.ProductRetrieveSerializer,
         'create': products_s.ProductCreateSerializer,
         'partial_update': products_s.ProductUpdateSerializer,
         'search': products_s.ProductSearchSerializer,
-        # 'destroy': products.ProductDeleteSerializer ???
     }
 
     http_method_names = ('get', 'post', 'patch', 'delete')
@@ -76,9 +80,8 @@ class ProductView(CRUDListViewSet):
         OrderingFilter,
         SearchFilter,
     )
-    ordering = ('is_available', 'id')
-
-    # filterset_class = ... ???
+    ordering = ('is_available', 'name', '-id')
+    search_fields = ('name',)
 
     @action(methods=['GET'], detail=False, url_path='search')
     def search(self, request: Request, *args: None, **kwargs: None) -> Response:
