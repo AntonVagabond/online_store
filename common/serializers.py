0d8f807e9e4b29e8ada8bd64dcc321098ce8037e
menma331 <romaxid@yandex.by>
2024-03-07 21:27:24 +0300

    feat: добавлена вложенность категорий с последующим выводом товаров.

--- common/serializers.py
+++ common/serializers.py
@@ -1,8 +1,7 @@
-from typing import OrderedDict, Union
+from products.models.categories import Category
+from products.serializers.api.products import ProductListSerializer
 
 from rest_framework import serializers
-
-from products.models.categories import Category
 
 
 class BaseCategorySerializer(serializers.ModelSerializer):
@@ -11,9 +10,20 @@ class BaseCategorySerializer(serializers.ModelSerializer):
     class Meta:
         abstract = True
 
-    def to_representation(self,
-                          instance: Category) -> OrderedDict[str, Union[int, str, None]]:
-        # Добавляем поле 'children' и его полное значение.
-        self.fields['children'] = self.__class__(many=True)
-        # Повторяется этот процесс, пока есть вложенность.
-        return super().to_representation(instance)
+    def to_representation(self, instance: Category) -> dict:
+        representation = super().to_representation(instance)
+
+        # Если у категории есть дочерние категории, рекурсивно сериализуем их.
+        children = instance.children.all()
+        if children:
+            representation['children'] = self.__class__(children, many=True).data
+
+        # Если у категории есть продукты, добавляем их в представление.
+        products = instance.products.all()
+        if products:
+            representation['products'] = ProductListSerializer(
+                instance.products.all(),
+                many=True
+            ).data
+
+        return representation
